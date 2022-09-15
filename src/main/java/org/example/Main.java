fd4dc7988f78450a8b9031486464d408b850f72c
OmidHdr <omidnet42@yahoo.com>
2022-09-16 01:15:40 +0430

    add switch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,12 @@ import org.example.repository.ConnectionManager;
 
 public class Main {
 
-    
-    public static void run(){
+    public static void run(String str){
         System.out.println("Runing ... ");
+        switch (str){
+            case "1":
+            System.out.println("press 'q' to quit : ");
+        }
     }
 
     public static void main(String[] args) {
