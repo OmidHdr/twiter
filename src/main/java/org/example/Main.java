c2343491c474660d9c756c725da8548a44436d92
OmidHdr <omidnet42@yahoo.com>
2022-09-15 22:57:03 +0430

    add class run

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,11 @@ import org.example.repository.ConnectionManager;
 
 public class Main {
 
+    
     public static void run(){
         System.out.println("Runing ... ");
     }
+
     public static void main(String[] args) {
         ConnectionManager.startDatabase();
         run();
